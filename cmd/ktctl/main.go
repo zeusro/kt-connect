91b40f67f6d1f31842584022bc4c20e1aeda842f
yunlzheng <yunl.zheng@gmail.com>
2020-02-20 22:42:30 +0800

    update version

--- cmd/ktctl/main.go
+++ cmd/ktctl/main.go
@@ -23,7 +23,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "KT Connect"
 	app.Usage = ""
-	app.Version = "master"
+	app.Version = "0.0.11.beta1"
 	app.Authors = command.NewCliAuthor()
 	app.Flags = command.AppFlags(options)
 	app.Commands = command.NewCommands(options)
